2c771daa5627ba393c5330df8f64c9c1a56dba8f
monterourena <davidmonterourena@gmail.com>
2024-07-25 01:07:43 -0600

    refactor: Enhance CircularButton component for scalability
    Refactored the CircularButton component to improve scalability. Changed the 'isSmall' prop to 'size' with options 'small' or 'regular'. Removed the ability to add a component via the children prop; now, only the icon name for the Icon component and its inherited props can be selected.
    
    BREAKING CHANGE: Replaced 'isSmall' prop with 'size'. Removed children prop in favor of icon name and inherited icon props.

--- src/components/common/Buttons/CircularButton.stories.tsx
+++ src/components/common/Buttons/CircularButton.stories.tsx
@@ -10,7 +10,7 @@ const meta = {
   parameters: {
     layout: 'centered'
   },
-  argTypes: {}
+  args: { iconName: 'ArrowUpRight' },
 } satisfies Meta<typeof CircularButton>
 
 export default meta
@@ -18,21 +18,21 @@ type Story = StoryObj<typeof meta>
 
 // STORIES
 export const DefaulSmall: Story = {
-  name: 'Default / Small',
-  args: { isSmall: true, isElevated: false }
+  name: 'Small (Default)',
+  args: { size: 'small', isElevated: false }
 }
 
 export const ElevatedSmall: Story = {
-  name: 'Elevated / Small',
-  args: { isSmall: true, isElevated: true }
+  name: 'Small (Elevated)',
+  args: { size: 'small', isElevated: true }
 }
 
 export const DefaultLarge: Story = {
-  name: 'Default / Regular',
-  args: { isSmall: false, isElevated: false }
+  name: 'Regular (Default)',
+  args: { size: 'regular', isElevated: false }
 }
 
 export const ElevatedRegular: Story = {
-  name: 'Elevated / Regular',
-  args: { isSmall: false, isElevated: true }
+  name: 'Regular (Elevated)',
+  args: { size: 'regular', isElevated: true }
 }
--- src/components/common/Buttons/CircularButton.tsx
+++ src/components/common/Buttons/CircularButton.tsx
@@ -1,23 +1,32 @@
-import { FC, PropsWithChildren } from 'react'
+import { FC } from 'react'
 import { style } from '@/utils/tailwind.utils'
+import Icon, { IconProps } from '@/components/common/Icons/Icon'
 
-interface CircularButtonProps extends PropsWithChildren {
-  isSmall?: boolean
+interface CircularButtonProps {
   isElevated?: boolean
+  iconName?: IconProps['name']
+  size?: 'small' | 'regular'
+  iconProps?: Omit<IconProps, 'name'>
 }
 
-const CircularButton: FC<CircularButtonProps> = ({ isSmall = false, isElevated, children }) => {
+const CircularButton: FC<CircularButtonProps> = ({ isElevated, size = 'regular', iconName = 'ArrowUpRight', iconProps }) => {
+
+  const containerStyles = style(
+    'bg-black-secondary border border-black-quaternary',
+    'flex justify-center items-center rounded-full',
+    size === 'small' && 'h-8 w-8',
+    size === 'regular' && 'h-12 w-12',
+    isElevated && 'bg-black-tertiary'
+  )
+
+  const iconStyles = style(
+    'stroke-white-quaternary',
+    size === 'small' && 'h-6 w-6',
+    size === 'regular' && 'h-8 w-8'
+  )
   return (
-    <button
-      className={style(
-        'bg-black-secondary border border-black-quaternary',
-        'justify-center items-center inline-flex',
-        'h-12 w-12 p-3 rounded-full',
-        isSmall && 'h-8 w-8 p-2',
-        isElevated && 'bg-black-tertiary'
-      )}
-    >
-      {children}
+    <button className={containerStyles}>
+      <Icon name={iconName} {...iconProps} className={iconStyles} />
     </button>
   )
 }
