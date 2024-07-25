6a8a254b948c781096e1dd6b960df079848fe2a5
monterourena <davidmonterourena@gmail.com>
2024-07-25 00:58:15 -0600

    feat: Update font sizes and border radius in tailwind.config.ts
    This commit updates the font sizes and border radius in the `tailwind.config.ts` file. It adds new font sizes for different elements and defines border radius values for various sizes. These changes enhance the styling options and improve the overall design consistency.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,21 +7,37 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
+    fontSize:{
+      largeTitle: '2.5rem',
+      title: '2rem',
+      subtitle: '1.5rem',
+      headline: '1.25rem',
+      body: '1rem',
+      caption: '0.75rem',
+    },
     colors: {
       black: {
         DEFAULT: '#000000',
         primary: '#060809',
         secondary: '#0C0E0F',
         tertiary: '#111314',
-        quaternary: '#1F1F1F',
+        quaternary: '#1F1F1F'
       },
       white: {
         DEFAULT: '#FFFFFF',
         primary: '#F9F9F9',
         secondary: '#D9D9D9',
         tertiary: '#ADADAD',
-        quaternary: '#777777',
+        quaternary: '#777777'
       }
+    },
+    borderRadius: {
+      DEFAULT: '0.25rem',
+      none: '0',
+      sm: '1rem',
+      md: '1.5rem',
+      lg: '2rem',
+      full: '9999px'
     },
     extend: {
       backgroundImage: {
