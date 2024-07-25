3540dd6568282b0de867b7ce210f52b6850653ba
monterourena <davidmonterourena@gmail.com>
2024-07-25 00:51:45 -0600

    docs: Add documentation to tailwind utilities

--- src/utils/tailwind.utils.ts
+++ src/utils/tailwind.utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
+/**
+ * Merges Tailwind CSS classes with the help of `clsx` and `tailwind-merge`.
+ *
+ * @param args - Tailwind CSS classes to merge.
+ * @returns - Merged Tailwind CSS classes.
+ */
 export function style(...args: ClassValue[]) {
-  return twMerge(clsx(args));
+  return twMerge(clsx(args))
 }
